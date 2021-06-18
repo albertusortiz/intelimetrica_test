ea89b16ba5769ebe5b7c720a3ce4cda96f53b013
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-06-18 01:17:55 -0500

    Getting data from table - Method READ

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+import pymysql
+
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 
@@ -9,15 +11,49 @@ load_dotenv()
 
 HOST = os.getenv('HOST')
 DATABASE = os.getenv('DATABASE')
-USER = os.getenv('USER')
+USERNAME_DATABASE = os.getenv('USERNAME_DATABASE')
 PASSWORD = os.getenv('PASSWORD')
+
+print("HOST:",HOST)
+print("DATABASE:",DATABASE)
+print("USERNAME_DATABASE:",USERNAME_DATABASE)
+print("PASWORD:",PASSWORD)
 
 app = Flask(__name__)
 
+miConexion = pymysql.connect(host=HOST,user=USERNAME_DATABASE,passwd=PASSWORD,db=DATABASE)
+cur = miConexion.cursor()
+
+
 @app.route("/")
 def index():
-    print("Welcome to Intelimetrica Test Backend")
     result = {'message': 'Index', 'status': '200', 'payload': []}
+    return jsonify(result), 200
+
+
+@app.route("/read")
+def read():
+    cur.execute("SELECT * FROM restaurants")
+    for row in cur.fetchall():
+        id = row[0]
+        name = row[2]
+        site = row[3]
+        # Now print fetched result
+        print ("id = {0}, name = {1}, site = {2}".format(id,name,site))
+    
+    restaurants = []
+    restaurants = cur.fetchall()
+
+    data_restaurants = {}
+    for row in cur.fetchall():
+        data_restaurants[row] = {
+            id : row[0],
+            name : row[2],
+            site : row[3],
+        }
+
+
+    result = {'message': 'Index', 'status': '200', 'payload': data_restaurants}
     return jsonify(result), 200
 
 
